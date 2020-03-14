6d8e5357e22c323cde01782a69700bbf020ef989
hicarty <>
2020-03-14 21:16:45 +0000

    -ADDED: WordPress Blog

--- Views/Home/Index.php
+++ Views/Home/Index.php
@@ -1,3 +1,7 @@
+<?php 
+    define('WP_USE_THEMES', false);
+    require('blog/wp-blog-header.php');
+?>
 <?php include("./includes/config.php");?>
 <div class='header'>
     <div class="page_ratio">
@@ -12,5 +16,16 @@
 <!-- body_text -->
 <div class="main_content">
     <div class="vb_album_container"></div>
+    <?php
+        $number_of_posts = 5;
+        $args = array( 'numberposts' => $number_of_posts );
+        $recent_posts = wp_get_recent_posts( $args );
+        foreach( $recent_posts as $recent_post ){
+        echo "<span>".$recent_post['post_date']."</span> <br>";
+        echo "<h3>".$recent_post['post_title']."</h3>";
+        echo  "<p>".$recent_post['post_content']."</p><br><br>";
+
+        }
+    ?>
 </div>
 
