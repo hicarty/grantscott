ccaced6dfb4a9eea4a7d5dfefde9bac2fa14894d
Haven Carty <>
2020-04-27 17:48:35 +0100

    -ADDED: Blade if statement

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -3,7 +3,7 @@
 </div>
 
 <!-- MENU -->
-<ul id="vb_menu"><li><a class='<?php if ($CURRENT_PAGE ?? '' == "Index") {?>active<?php }?>' href="." >Home</a></li><li><a class='<?php if ($CURRENT_PAGE ?? '' == "People") {?>active<?php }?>' href="./people.php" >People</a></li><li><a class='<?php if ($CURRENT_PAGE ?? '' == "Places and Spaces") {?>active<?php }?>' href="./places-and-spaces.php" >Places and Spaces</a></li><li><a class='<?php if ($CURRENT_PAGE ?? '' == "Projects") {?>active<?php }?>' href="./projects.php" >Projects</a></li><li><a href="./films.php" target="_blank">Films</a></li><li><a href="./blog/index.pho" target="_blank">Blog</a></li><li><a class='<?php if ($CURRENT_PAGE ?? '' == "About") {?>active<?php }?>' href="./about.php" >About</a></li><li><a class='<?php if ($CURRENT_PAGE ?? '' == "Contact") {?>active<?php }?>' href="./contact.php" >Contact</a></li></ul>
+<ul id="vb_menu"><li><a class='<?php if ($CURRENT_PAGE ?? '' == "Index") {?>active<?php }?>' href="./" >Home</a></li><li><a class='<?php if ($CURRENT_PAGE ?? '' == "People") {?>active<?php }?>' href="./people/" >People</a></li><li><a class='<?php if ($CURRENT_PAGE ?? '' == "Places and Spaces") {?>active<?php }?>' href="./places-and-spaces" >Places and Spaces</a></li><li><a class='<?php if ($CURRENT_PAGE ?? '' == "Projects") {?>active<?php }?>' href="./projects" >Projects</a></li><li><a href="./films" target="_blank">Films</a></li><li><a href="./blog/index.pho" target="_blank">Blog</a></li><li><a class='<?php if ($CURRENT_PAGE ?? '' == "About") {?>active<?php }?>' href="./about" >About</a></li><li><a class='<?php if ($CURRENT_PAGE ?? '' == "Contact") {?>active<?php }?>' href="./contact" >Contact</a></li></ul>
 
 <!-- SIDE -->
 <div id="vb_info_container">
@@ -11,8 +11,8 @@
     <h2>&nbsp;</h2>
     <!-- SIDE_TEXT -->
     <div id="vb_info_content">
-    <?php if ($CURRENT_PAGE ?? '' == "Index") { ?>
+    @if ($CURRENT_PAGE ?? '' == "Index")
         <br><br><br><br>&copy; All images copyright Charles Mooney <?php print(gmdate("Y"));?>
-    <?php } ?>
+    @endif
     </div>
 </div>
