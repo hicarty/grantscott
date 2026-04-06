fe5fdfb949c90e64763a0c65f5ef96254a5c51f3
Haven <havencarty@outlook.com>
2026-04-06 09:39:22 +0000

    -RENAMED: For PHP naming conventions

--- includes/config.php
+++ includes/config.php
@@ -1,23 +1,19 @@
 <?php
-	switch ($_SERVER["HTTP_HOST"]) {
-		case "./contact.php":
-			$CURRENT_PAGE = "Contact"; 
-			$PAGE_TITLE = "Contact Us";
-			break;
-		case "./about.php":
-			$CURRENT_PAGE = "About"; 
-			$PAGE_TITLE = "About Us";
-			break;
-		case "./projects.php":
-			$CURRENT_PAGE = "Projects"; 
-			$PAGE_TITLE = "Projects";
-			break;
-		case "./people.php":
-			$CURRENT_PAGE = "People"; 
-			$PAGE_TITLE = "People";
-			break;
-		default:
-			$CURRENT_PAGE = "Index";
-			$PAGE_TITLE = "Home";
+	$uri = $_SERVER['REQUEST_URI'];
+	if (strpos($uri, '/contact') !== false) {
+		$CURRENT_PAGE = "Contact"; 
+		$PAGE_TITLE = "Contact Us";
+	} elseif (strpos($uri, '/about') !== false) {
+		$CURRENT_PAGE = "About"; 
+		$PAGE_TITLE = "About Us";
+	} elseif (strpos($uri, '/projects') !== false) {
+		$CURRENT_PAGE = "Projects"; 
+		$PAGE_TITLE = "Projects";
+	} elseif (strpos($uri, '/people') !== false) {
+		$CURRENT_PAGE = "People"; 
+		$PAGE_TITLE = "People";
+	} else {
+		$CURRENT_PAGE = "Index";
+		$PAGE_TITLE = "Home";
 	}
 ?>
